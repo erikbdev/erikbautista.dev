84dc973db051d8c9d4bc05374b4e0a9810f344aa
ErrorErrorError <16653389+ErrorErrorError@users.noreply.github.com>
2023-09-02 15:18:57 -0500

    wip: add support for home page and info

---
+++ app/components/footer.tsx
@@ -0,0 +1,13 @@
+
+export function Footer() {
+    return (
+        <footer>
+            <div className="w-full h-0.5 bg-neutral-500/25"></div>
+            <div className="mx-auto px-24 py-8 grid grid-cols-3">
+                <p className="justify-self-center">Home</p>
+                <p className="justify-self-center">Projects</p>
+                <p className="">About</p>
+            </div>
+        </footer>
+    )
+}
