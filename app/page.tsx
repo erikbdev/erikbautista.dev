c3a33f33bacf5e9c102227c3a3bfb46fed2410bb
ErrorErrorError <16653389+ErrorErrorError@users.noreply.github.com>
2023-09-03 04:37:21 -0500

    wip: improve mobile site

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,68 @@
-import Image from 'next/image'
-import { Footer } from './components/footer'
+const socialMedia: Social[] = [];
+
+const projectButtons: Project[] = [
+  {
+    title: "Title 1",
+    description: "Some Description"
+  },
+  {
+    title: "Title 2",
+    description: "Some Description"
+  },
+];
 
 export default function Home() {
   return (
-    <main>
-      <div className='container mx-auto md:grid md:grid-cols-2 md:gap-8 p-20 h-screen content-center'>
-        {/* Name */}
-        <div className='self-center'>
-          <p className='font-semibold text-3xl md:text-4xl'>Erik Bautista Santibanez</p>
-          <p className='my-2 text-lg text-gray-300'>iOS Developer and Software Engineer</p>
+    <div className="h-screen container max-w-6xl mx-auto p-8 grid space-y-8">
+      <div className='row-span-6 md:grid md:grid-cols-2 md:gap-8 h-full content-center'>
+        <div className="pb-8 self-center">
+          {/* Name & Title */}
+          <div>
+            <p className='font-semibold text-3xl'>Erik Bautista Santibanez</p>
+            <p className='my-2 text-lg text-gray-300'>iOS Developer & Software Engineer</p>
+          </div>
+          {/* TODO: Add buttons to find me */}
+          <div>
+            {socialMedia.map(SocialButton)}
+          </div>
         </div>
         <div className='self-center w-fill'>
-          <div className='flex flex-col'>
-            { ActionButton() }
-            { ActionButton() }
+          <div className='flex flex-col space-y-4'>
+            {projectButtons.map(ProjectButton)}
           </div>
         </div>
       </div>
-      {/* Footer
-      { Footer() } */}
-    </main>
+
+      {/* Footer */}
+      <div className="mx-auto md:mx-0 self-end">
+        <p className="text-sm text-neutral-400">© 2023 Erik Bautista Santibanez. All rights reserved.</p>
+      </div>
+    </div>
   )
 }
 
-function ActionButton() {
+const SocialButton = (social: Social) => {
+  return (<div></div>)
+}
+
+const ProjectButton = (project: Project) => {
   return (
-    <div className='my-2 w-fill bg-neutral-400 h-24'>
-      <p>Title 1</p>
-      <p>Some description</p>
-    </div>
+    <button className="bg-neutral-500/40 rounded-lg h-24">
+      <div className="px-6 text-white text-left">
+        <p className="text-lg font-semibold">{project.title}</p>
+        <p className="text-sm font-normal">{project.description}</p>
+      </div>
+    </button>
   )
+}
+
+interface Social {
+  name: string,
+  image: string,
+  link: string
+}
+
+interface Project {
+  title: string,
+  description: string
 }
