bb2003665dbec1489412eab181a0c36b4bdb7ab0
ErrorErrorError <16653389+ErrorErrorError@users.noreply.github.com>
2024-05-21 23:20:57 -0700

    install swift

--- scripts/swift-install.sh
+++ scripts/swift-install.sh
@@ -3,4 +3,6 @@ if swift --version &> /dev/null; then
 else
   echo "warning: Swift is not installed, installing swiftly to install latest Swift version.";
   curl -L https://swift-server.github.io/swiftly/swiftly-install.sh | bash -s -- -y
+  . $HOME/.local/share/swiftly/env.sh
+  swiftly install latest
 fi
