2253c4b77cfddc7dda4d9b72338d9899bff758ae
ErrorErrorError <16653389+ErrorErrorError@users.noreply.github.com>
2024-05-21 23:15:53 -0700

    test install swift to vercel

--- api/app.swift
+++ api/app.swift
@@ -1,14 +1,12 @@
-// import VercelMacros
-// import VercelRuntime
 import Vercel
 
 @main
-// @Routable
 struct App: ExpressHandler {
   static var basePath = "/api"
-    static func configure(router: isolated Router) async throws {
-      router.get("/") { req, res in
-        res.status(.ok).send("Hello, Swift")
-      }
+
+  static func configure(router: isolated Router) async throws {
+    router.get("/") { req, res in
+      res.status(.ok).send("Hello, Swift")
     }
+  }
 }
---
+++ scripts/swift-install.sh
@@ -0,0 +1,6 @@
+if swift --version &> /dev/null; then
+  echo "Swift is currently installed, skipping installation.";
+else
+  echo "warning: Swift is not installed, installing swiftly to install latest Swift version.";
+  curl -L https://swift-server.github.io/swiftly/swiftly-install.sh | bash -s -- -y
+fi
