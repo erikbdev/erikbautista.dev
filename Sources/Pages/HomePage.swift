702e9615c721e047cb8b6f5c503606f16e08eedc
Erik Bautista Santibanez <erikbautista15@gmail.com>
2025-02-02 21:52:23 -0800

    wip: add articles

--- Sources/Pages/HomePage.swift
+++ Sources/Pages/HomePage.swift
@@ -7,15 +7,26 @@ import SyntaxHighlight
 
 public struct HomePage: Page {
   struct Style: Sendable, StyleSheet {
-    let container = Class(module: "container")
     let svgIcon = Class(module: "svgIcon")
     let reversed = Class(module: "reversed")
-    let location = Class(module: "location")
-    let educationItem = Class(module: "eductation-item")
 
     var body: some Rule {
-      Attribute("data-theme", match: .exact, value: "dark") => {
-        Variable("pico-background-color", value: "#1c1c1c")
+      Import(.url("https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap"))
+
+      Pseudo(class: .root) => {
+        BackgroundColor("#1c1c1c")
+        Color("#fafafa")
+        AnyProperty("font-family", "\"Work Sans\", serif")
+        AnyProperty("font-optical-sizing", "auto")
+        AnyProperty("font-style", "normal")
+      }
+
+      Element(.body) => {
+        AnyProperty("max-width", "640px")
+        AnyProperty("margin-right", "auto")
+        AnyProperty("margin-left", "auto")
+        AnyProperty("padding-left", "24px")
+        AnyProperty("padding-right", "24px")
       }
 
       self.svgIcon => {
@@ -31,15 +42,6 @@ public struct HomePage: Page {
       self.reversed => {
         AnyProperty("scale", "calc(100% * -1) 100%")
       }
-
-      // self.location > Element(.b) => {
-      //   Color("#fff")
-      // }
-
-      self.educationItem => {
-        Display(.flex)
-        AnyProperty("justify-content", "space-between")
-      }
     }
   }
 
@@ -47,14 +49,21 @@ public struct HomePage: Page {
 
   public init() {}
  
-  private static let yearDateFormatter = DateFormatter()
+  private static let articleDateFormatter = {
+    let formatter = DateFormatter()
+    formatter.locale = Locale(languageCode: .english, languageRegion: .unitedStates)
+    formatter.timeZone = TimeZone(abbreviation: "PST") ?? formatter.timeZone
+    formatter.dateFormat = "MMM, d yyyy"
+    return formatter
+  }()
 
-  var year: String {
-    Self.yearDateFormatter.locale = Locale(languageCode: .english, languageRegion: .unitedStates)
-    Self.yearDateFormatter.timeZone = TimeZone(abbreviation: "PST") ?? Self.yearDateFormatter.timeZone
-    Self.yearDateFormatter.dateFormat = "yyyy"
-    return Self.yearDateFormatter.string(from: Date.now)
-  }
+  private static let copyrightDateFormatter = {
+    let formatter = DateFormatter()
+    formatter.locale = Locale(languageCode: .english, languageRegion: .unitedStates)
+    formatter.timeZone = TimeZone(abbreviation: "PST") ?? formatter.timeZone
+    formatter.dateFormat = "yyyy"
+    return formatter
+  }()
 
   let styling = Style()
 
@@ -65,11 +74,14 @@ public struct HomePage: Page {
         title { "Erik Bautista Santibanez" }
         meta(.charset(.utf8))
         meta(name: .viewport, content: "width=device-width, initial-scale=1.0")
-        // link(.rel(.stylesheet), .href("https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"))
+        script(.src("https://cdn.jsdelivr.net/npm/alpinejs@3.14.8/dist/cdn.min.js"), .defer)
         style(styling)
-        // script(.src("https://cdn.jsdelivr.net/npm/alpinejs@3.14.8/dist/cdn.min.js"), .defer) {}
+        style {
+          // ""
+        }
+        // link(.href("https://fonts.googleapis.com/css?family=Archivo:400,400i,500,500i,600,600i,700,700i"), .rel(.stylesheet))
       }
-      body(.class("container")) {
+      body {
         header(.ariaLabel("About")) {
           hgroup {
             h1 { "Erik Bautista Santibanez" }
@@ -91,63 +103,59 @@ public struct HomePage: Page {
               if let location, location.city != residency.city || location.state != residency.state {
                 " \u{2022} "
 
-                span(.ariaLabel("Location"), .class(styling.location)) {
+                span(.ariaLabel("Location")) {
                   svg(.xmlns(), .fill("currentColor"), .viewBox("0 0 256 256"), .class(styling.svgIcon), .class(styling.reversed), .ariaLabel("Navigation icon")) {
                     path(.d("M234.35,129,152,152,129,234.35a8,8,0,0,1-15.21.27l-65.28-176A8,8,0,0,1,58.63,48.46l176,65.28A8,8,0,0,1,234.35,129Z"))
                     path(.d("M237.33,106.21,61.41,41l-.16-.05A16,16,0,0,0,40.9,61.25a1,1,0,0,0,.05.16l65.26,175.92A15.77,15.77,0,0,0,121.28,248h.3a15.77,15.77,0,0,0,15-11.29l.06-.2,21.84-78,78-21.84.2-.06a16,16,0,0,0,.62-30.38ZM149.84,144.3a8,8,0,0,0-5.54,5.54L121.3,232l-.06-.17L56,56l175.82,65.22.16.06Z"))
                   }
 
                   "Currently in "
-
-                  // span {
-                    b {
-                      [location.city, location.state, location.region == "United States" ? nil : location.region]
-                        .compactMap(\.self)
-                        .joined(separator: ", ")
-                    }
-                  // }
+                  
+                  b {
+                    [location.city, location.state, location.region == "United States" ? nil : location.region]
+                      .compactMap(\.self)
+                      .joined(separator: ", ")
+                  }
                 }
               }
             }
           }
         }
-        main {
-          article {
-            // header {
-            //   a(.target(.blank), .rel("noopener noreferrer"), .href("https://stedwards.edu/")) {
-            //     "St. Edward's University"
-            //   }
-            // }
-            // p { "Bachelor of Science in Computer Science" }
-            h6 { "Graduation" }
-            p  { "I graduated from St. Edward's University with a Bachelor of Science in Computer Science." }
-          }
-          // TODO: Do a timeline with filters
-          // section(.ariaLabel("Experience")) {
-          //   h5 { "Experience" }
-          // }
-          // section(.ariaLabel("Projects")) {
-          //   h5 { "Projects" }
-          // }
-          // section(.ariaLabel("Education")) {
-          //   h5 { "Education" }
-          //   div(.class(styling.educationItem)) {
-          //     div {
-          //       a(.target(.blank), .rel("noopener noreferrer"), .href("https://stedwards.edu/")) {
-          //         "St. Edward's University"
-          //       }
-          //       p { 
-          //         i { "Bachelor of Science in Computer Science" }
-          //       }
-          //     }
+        main(.x.data("{ selected: \"all\" }")) {
+          ul {
+            li { "All" }
 
-          //     p { "2018-2023" }
-          //   }
-          // }
+            for kind in Article.Kind.allCases {
+              li { HTMLText(kind.rawValue) }
+            }
+          }
+
+          for event in Article.allCases {
+            article {
+              header { Self.articleDateFormatter.string(from: event.date) }
+              // if let hero = event.hero {
+              //   figure {
+              //     figcaption {}
+              //   }
+              // }
+
+              h3 {
+                HTMLText(event.title)
+              }
+
+              p {
+                HTMLRaw(event.description)
+              }
+
+              // if let actions = event.actionButtons {
+                
+              // }
+            }
+          }
         }
         footer(.ariaLabel("Credits")) {
           hr()
-          p { "©\(self.year) Erik Bautista Santibanez" }
+          p { "©\(Self.copyrightDateFormatter.string(from: Date.now)) Erik Bautista Santibanez" }
           p {
             "Made with \u{2764} using "
             a(.target(.blank), .rel("noopener noreferrer"), .href("https://swift.org")) { "Swift" }
@@ -179,7 +187,57 @@ public struct HomePage: Page {
 //   }
 // }
 
-struct Event {
-  let date: Date
-  let title: String
+struct Article {
+  var hero: Hero?
+  var title: String
+  var description: String
+  var date: Date
+  var kind: Kind
+  var actionButtons: [LinkButton]?
+
+  enum Hero {
+    case link(URL)
+    case image(String)
+    case video(String)
+    case code(String)
+  }
+
+  struct LinkButton {
+    let title: String
+    let link: String
+  }
+
+  enum Kind: String, Hashable, CaseIterable {
+    case project = "Project"
+    case education = "Education"
+    case experience = "Experiences"
+  }
+}
+
+extension Article: CaseIterable {
+  static let allCases: [Article] = [
+    Self(
+      title: "A Swift DSL for type-safe CSS",
+      description: """
+      Since I started building this website using Swift, I used [elementary](https://github.com/sliemeobn/elementary) to build \
+      HTML pages.
+
+      TBD
+      """,
+      date: Date(timeIntervalSince1970: 1738483200),  // Feb 2, 2025
+      kind: .project
+    ),
+    Self(
+      title: "Anime Now! \u{2014} An iOS and macOS App",
+      description: "TBD",
+      date: Date(timeIntervalSince1970: 1663225200),  // Sep 15, 2025
+      kind: .project
+    ),
+    Self(
+      title: "PrismUI \u{2014} Controlling MSI RGB Keyboard on mac",
+      description: "TBD",
+      date: Date(timeIntervalSince1970: 1663225200),
+      kind: .project
+    )
+  ]
 }
