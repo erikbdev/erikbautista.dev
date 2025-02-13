071208bfa9105a34a7cb1031c4ed63800b740c5e
Erik Bautista Santibanez <erikbautista15@gmail.com>
2025-02-12 20:06:19 -0800

    wip: improve border lines

--- Sources/Pages/HomePage.swift
+++ Sources/Pages/HomePage.swift
@@ -19,9 +19,9 @@ public struct HomePage: Page {
 
   public var content: some HTML {
     HTMLRaw("<!DOCTYPE html>")
-    html(.lang("en"), .data("theme", value: "dark")) {
+    html(.lang("en")) {
       head {
-        title { "Erik Bautista Santibanez" }
+        title { "Erik Bautista Santibanez | Portfolio" }
         meta(.charset(.utf8))
         meta(name: .viewport, content: "width=device-width, initial-scale=1.0")
         link(.rel(.stylesheet), .href("https://cdnjs.cloudflare.com/ajax/libs/modern-normalize/3.0.1/modern-normalize.min.css"))
@@ -32,20 +32,26 @@ public struct HomePage: Page {
         script { HTMLRaw("hljs.highlightAll();") }
         VueScript()
       }
-      body {
-        // TODO: Add header to switch between posts and swich from cod?
+      body(.v.scope("{ showCode: true }")) {
+        header(.class("wrapped")) {
+          // TODO: Allow changing from swift-syntax based language to regular
+          div(.class("container"), .style("height: 3rem")) {}
+        }
         main {
           UserHero()
+          // ExperienceSection()
           PostsSection()
         }
-        footer(.aria.label("Credits"), .style("text-align: center;")) {
-          p { "©\(Self.copyrightDateFormatter.string(from: Date.now)) Erik Bautista Santibanez" }
-          p {
-            "Made with \u{2764} using "
-            a(.target(.blank), .rel("noopener noreferrer"), .href("https://swift.org")) { "Swift" }
-            " + "
-            a(.target(.blank), .rel("noopener noreferrer"), .href("https://hummingbird.codes")) { "Hummingbird" }
-            "."
+        footer(.aria.label("Credits"), .class("wrapped"), .style("text-align: center;")) {
+          div(.id("end-footer"), .class("container")) {
+            p { "©\(Self.copyrightDateFormatter.string(from: Date.now)) Erik Bautista Santibanez" }
+            p {
+              "Made with \u{2764} using "
+              a(.target(.blank), .rel("noopener noreferrer"), .href("https://swift.org")) { "Swift" }
+              " + "
+              a(.target(.blank), .rel("noopener noreferrer"), .href("https://hummingbird.codes")) { "Hummingbird" }
+              "."
+            }
           }
         }
       }
@@ -56,58 +62,51 @@ public struct HomePage: Page {
     @Dependency(\.activityClient) private var activityClient
 
     var content: some HTML {
-      section(.class("hero container"), .aria.label("About")) {
-        p(.class("file-name")) { "User.swift" }
-        hgroup(.class("container-content")) {
-          // h1(.class("hero-title")) { "Erik Bautista Santibanez" }
-          // p(.class("hero-subtitle")) { "Mobile & Web Developer" }
-          // p { residency }
-          // p { location }
+      section(.id("hero"), .class("wrapped"), .aria.label("About")) {
+        div(.class("container")) {
+          code(.class("code-tag")) { "User.swift" }
 
-          pre {
-            code(.data("highlighted", value: "yes"), .class("hljs language-swift")) {
-              span(.class("hljs-comment")) { "/// User.swift\n" }
-              span(.class("hljs-comment")) { "/// Portfolio\n" }
-              span(.class("hljs-comment")) { "/// \n" }
-              span(.class("hljs-keyword")) { "struct" }
-              " "
-              span(.class("hljs-title class_")) { "User" }
-              ": "
-              span(.class("hljs-title class_")) { "Portfolio" }
-              " {\n  "
-              span(.class("hljs-keyword")) { "var" }
-              " body: "
-              span(.class("hljs-keyword")) { "some" }
-              " "
-              span(.class("hljs-type")) { "Description" }
-              " { \n    "
-              span(.class("hljs-type")) { "Name" }
-              "("
-              span(.class("hljs-string")) {
-                "\""
-                span(.class("hero-title")) { "Erik Bautista Santibanez" }
-                "\""
+          header(.class("code-header")) {
+            pre {
+              code(.data("highlighted", value: "yes"), .class("hljs language-swift")) {
+                span(.class("hljs-comment")) { "/// User.swift\n" }
+                span(.class("hljs-comment")) { "/// Portfolio\n" }
+                span(.class("hljs-keyword")) { "struct" }
+                " "
+                span(.class("hljs-title class_")) { "User" }
+                ": "
+                span(.class("hljs-title class_")) { "Portfolio" }
+                " {\n  "
+
+                span(.class("hljs-keyword")) { "let" }
+                " name = "
+                span(.class("hljs-string")) {
+                  "\""
+                  span(.class("hero-title")) { "Erik Bautista Santibanez" }
+                  "\""
+                }
+                ";\n  "
+
+                span(.class("hljs-keyword")) { "let" }
+                " role = "
+                span(.class("hljs-string")) {
+                  "\""
+                  span(.class("hero-subtitle")) { "Mobile & Web Developer" }
+                  "\""
+                }
+                ";\n  "
+
+                span(.class("hljs-keyword")) { "let" }
+                " home = "
+                span(.class("hljs-string")) {
+                  "\""
+                  span(.class("hero__location")) { self.residency }
+                  "\""
+                }
+                ";\n"
+                self.location
+                "}"
               }
-              ")\n    "
-              span(.class("hljs-type")) { "Role" }
-              "("
-              span(.class("hljs-string")) {
-                "\""
-                span(.class("hero-subtitle")) { "Mobile & Web Developer" }
-                "\""
-              }
-              ")\n    "
-              span(.class("hljs-type")) { "Home" }
-              "("
-              span(.class("hljs-string")) { 
-                "\""
-                span(.class("hero-location")) { self.residency }
-                "\""
-              }
-              ")\n"
-              location
-              HTMLRaw("  }\n")
-              "}"
             }
           }
         }
@@ -133,12 +132,12 @@ public struct HomePage: Page {
       let residency = location?.residency ?? .default
 
       if let location, location.city != residency.city || location.state != residency.state {
-        "    "
-        span(.class("hljs-type")) { "Location" }
-        "("
-        span(.class("hljs-string")) { 
+        "  "
+        span(.class("hljs-keyword")) { "let" }
+        " location = "
+        span(.class("hljs-string")) {
           "\""
-          span(.class("hero-location"), .aria.label("Location")) {
+          span(.class("hero__location"), .aria.label("Location")) {
             svg(.xmlns(), .fill("currentColor"), .viewBox("0 0 256 256"), .class("svg-icon reversed"), .aria.label("Navigation icon")) {
               path(.d("M234.35,129,152,152,129,234.35a8,8,0,0,1-15.21.27l-65.28-176A8,8,0,0,1,58.63,48.46l176,65.28A8,8,0,0,1,234.35,129Z"))
               path(.d("M237.33,106.21,61.41,41l-.16-.05A16,16,0,0,0,40.9,61.25a1,1,0,0,0,.05.16l65.26,175.92A15.77,15.77,0,0,0,121.28,248h.3a15.77,15.77,0,0,0,15-11.29l.06-.2,21.84-78,78-21.84.2-.06a16,16,0,0,0,.62-30.38ZM149.84,144.3a8,8,0,0,0-5.54,5.54L121.3,232l-.06-.17L56,56l175.82,65.22.16.06Z"))
@@ -152,9 +151,8 @@ public struct HomePage: Page {
                 .joined(separator: ", ")
             }
           }
-          "\""
+          "\"\n"
         }
-        ")\n"
       } else {
         EmptyHTML()
       }
@@ -163,42 +161,81 @@ public struct HomePage: Page {
 
   private struct PostsSection: HTML {
     var content: some HTML {
-      section(.class("container"), .v.scope("{ selection: undefined }")) {
-        header {
-          p(.class("file-name")) { "Posts.swift" }
-          hgroup(.class("container-content")) {
-            h2 { "Posts" }
-            ul(.class("post-tabs")) {
-              for kind in Post.Kind?.allCases {
-                let value = if let kind {
-                  "'\(kind.rawValue)'"
-                } else {
-                  "undefined"
-                }
-                li {
-                  button(
-                    .class("post-tab"),
-                    .v.on(.click, "selection = \(value)"),
-                    .v.bind("aria-selected", "selection == \(value)"),
-                    .aria.selected(kind == nil)
-                  ) {
-                    kind?.tabTitle ?? "All"
-                  }
-                }
+      section(.id("posts"), .class("wrapped"), .v.scope("{ selection: undefined }")) {
+        div(.class("container")) {
+          code(.class("code-tag")) { "Posts.swift" }
+
+          header(.class("code-header")) {
+            pre(.class("hljs language-swift")) {
+              code {
+                """
+                /// Posts.swift
+                /// Portfolio
+                var posts: [Post]
+                """
               }
             }
+            // ul(.class("posts__tab")) {
+            //   for kind in Post.Kind?.allCases {
+            //     let value = if let kind {
+            //       "'\(kind.rawValue)'"
+            //     } else {
+            //       "undefined"
+            //     }
+            //     li {
+            //       button(
+            //         .class("posts__tab-item"),
+            //         .v.on(.click, "selection = \(value)"),
+            //         .v.bind("aria-selected", "selection == \(value)"),
+            //         .aria.selected(kind == nil)
+            //       ) {
+            //         kind?.tabTitle ?? "All"
+            //       }
+            //     }
+            //   }
+            // }
           }
-        }
 
-        section(.class("container-content")) {
-          for post in Post.allCases {
+          for (idx, post) in Post.allCases.enumerated() {
             article(
               .class("post"),
               .v.show("!selection || selection == '\(post.kind.rawValue)'")
             ) {
-              header(.class("post-date")) { post.dateFormatted }
-              h3(.class("post-title")) { post.title }
-              section(.class("post-content")) { post.content }
+              header {
+                div(.style("display: flex")) {
+                  span(.class("post__date"), .style("flex-grow: 1")) { post.dateFormatted }
+
+                  pre { 
+                    code(.class("hljs language-swift"), .style("font-size: 0.75em; color: #777; font-weight: 500")) {
+                      "post[\(idx)]"
+                    } 
+                  }
+                }
+              }
+              h3(.class("post__time")) { post.title }
+              section(.class("post__content")) { post.content }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  private struct ExperienceSection: HTML {
+    var content: some HTML {
+      section(.id("experience"), .class("wrapped")) {
+        div(.class("container")) {
+          code(.class("code-tag")) { "Experience.swift" }
+
+          header(.class("code-header")) {
+            pre(.class("hljs language-swift")) {
+              code {
+                """
+                /// Experience.swift
+                /// Portfolio
+                var experiences: [Post]
+                """
+              }
             }
           }
         }
@@ -240,8 +277,6 @@ extension HomePage {
     }
 
     @CSSBuilder func generalStyles() -> some Rule {
-      // TODO: Add Work-Sans font?
-
       // MARK: - General
 
       "@font-face" => {
@@ -257,38 +292,39 @@ extension HomePage {
         Color("#fafafa")
         AnyProperty("font-optical-sizing", "auto")
         AnyProperty("font-style", "normal")
+        AnyProperty("font-size", "0.9em")
       }
 
-      Element(.body) => {
-        // AnyProperty("margin-top", "2rem")
-        // AnyProperty("margin-bottom", "3rem")
-      }
-
-      Element(.body) > All() => {
-        AnyProperty("max-width", "40rem")
-        AnyProperty("margin-right", "auto")
-        AnyProperty("margin-left", "auto")
+      Class("wrapped") => {
+        AnyProperty("border-top", "1px solid #303030")
       }
 
       Class("container") => {
-        AnyProperty("border-top", "1px solid #303030")
+        AnyProperty("max-width", "46rem")
+        AnyProperty("margin-right", "auto")
+        AnyProperty("margin-left", "auto")
         AnyProperty("border-left", "1px solid #303030")
         AnyProperty("border-right", "1px solid #303030")
       }
 
-      Class("container-content") => {
-        // AnyProperty("padding-top", "1.25rem")
-        AnyProperty("padding-left", "1.5rem")
-        AnyProperty("padding-right", "1.5rem")
-      }
-
-      Class("file-name") => {
+      Class("code-tag") => {
+        AnyProperty("display", "block")
         Color("#777")
         AnyProperty("text-align", "end")
         AnyProperty("font-size", "0.75em")
         AnyProperty("font-weight", "500")
         AnyProperty("font-family", "\"CommitMono\", monospace")
-        AnyProperty("padding", "0.5rem 1.5rem 0.5rem 1.5rem")
+
+        AnyProperty("padding-top", "1.5rem")
+        AnyProperty("padding-left", "1.5rem")
+        AnyProperty("padding-right", "1.5rem")
+        AnyProperty("padding-bottom", "0.75rem")
+      }
+
+      Class("code-header") => {
+        AnyProperty("padding-left", "1.5rem")
+        AnyProperty("padding-right", "1.5rem")
+        AnyProperty("padding-bottom", "1.5rem")
       }
 
       Class("svg-icon") => {
@@ -305,47 +341,46 @@ extension HomePage {
         AnyProperty("scale", "calc(100% * -1) 100%")
       }
 
-      Element(.code) => {
+      "code" => {
         AnyProperty("font-family", "\"CommitMono\", monospace")
         AnyProperty("font-feature-settings", "\"ss03\", \"ss04\", \"ss05\"")
         AnyProperty("line-height", "1")
+      }
+
+      ID("end-footer") => {
+        AnyProperty("padding-top", "1rem")
+        AnyProperty("padding-left", "1.5rem")
+        AnyProperty("padding-right", "1.5rem")
+        AnyProperty("padding-bottom", "1rem")
       }
     }
 
     @CSSBuilder func heroStyles() -> some Rule {
       // MARK: - Hero header
 
-      Class("hero") => {
-        AnyProperty("padding-bottom", "1.5rem")
-        AnyProperty("font-size", "0.9em")
-      }
+      ID("hero") => {}
 
-      // ".hero-title, .hero-subtitle, .hero-location" => {
-      //   // AnyProperty("all", "revert")
-      //   AnyProperty("display", "inline")
-      //   AnyProperty("font-family", "system-ui, 'Seggoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji'")
-      // }
-      
-
-      Class("hero-location") => {
+      Class("hero__location") => {
         Color(.hex("#D0D0D0"))
       }
     }
 
     @CSSBuilder func postTabsStyles() -> some Rule {
-      Class("post-tabs") => {
+      ID("posts") => {}
+
+      Class("posts__tab") => {
         AnyProperty("list-style-type", "none")
         AnyProperty("margin", "0")
         AnyProperty("padding", "0")
         AnyProperty("overflow", "hidden")
       }
 
-      Class("post-tabs") > Element(.li) => {
+      Class("posts__tab") > Element(.li) => {
         AnyProperty("float", "left")
         AnyProperty("margin-right", "0.25rem")
       }
 
-      Class("post-tab") => {
+      Class("posts__tab-item") => {
         BackgroundColor("#3c3c3c")
         AnyProperty("color", "white")
         AnyProperty("border-color", "#505050")
@@ -355,7 +390,7 @@ extension HomePage {
         AnyProperty("padding", "0.25rem 0.75rem")
       }
 
-      Class("post-tab") <> .attr("aria-selected", match: .exact, value: "true") => {
+      Class("posts__tab-item") <> .attr("aria-selected", match: .exact, value: "true") => {
         AnyProperty("background-color", "#F0F0F0")
         AnyProperty("color", "#101010")
         AnyProperty("border-color", "#A0A0A0")
@@ -363,29 +398,32 @@ extension HomePage {
     }
 
     @CSSBuilder func postStyles() -> some Rule {
-      // MARK: - Posts
+      // MARK: - Post
 
       Class("post") => {
-        AnyProperty("margin-top", "0.75rem")
-        AnyProperty("margin-bottom", "1.5rem")
-        // AnyProperty("border-bottom", "1px dotted #404040")
+        AnyProperty("width", "100%")
+        AnyProperty("display", "inline-block")
+        AnyProperty("padding", "1.5rem")
+        AnyProperty("background-image", "repeating-linear-gradient(90deg,#444 0 15px,transparent 0 30px)")
+        AnyProperty("background-repeat", "repeat-x")
+        AnyProperty("background-size", "100% 1px")
       }
 
-      Class("post-date") => {
+      Class("post__date") => {
         Color("#9A9A9A")
         AnyProperty("font-size", "0.75em")
         AnyProperty("font-weight", "600")
       }
 
-      Class("post-title") => {
+      Class("post__time") => {
         AnyProperty("margin-top", "0.5rem")
       }
 
-      Class("post-content") * All() => {
+      Class("post__content") * All() => {
         AnyProperty("margin", "revert")
       }
 
-      Class("post") * Element(.pre) => {
+      Class("post__content") * Element(.pre) => {
         AnyProperty("padding", "0.75rem")
         Background("#242424")
         AnyProperty("border-color", "#3A3A3A")
